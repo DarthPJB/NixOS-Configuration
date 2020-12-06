7aa44bf0b63fc5dfe593898bc467acc4d649816d
John Bargman <darthpjb@gmail.com>
2020-12-06 21:26:40 +0000

    corrected a few more errors

--- config/enviroments/i3wm.nix
+++ config/enviroments/i3wm.nix
@@ -13,7 +13,7 @@
         {
             enable = true;
             package = "pkgs.i3-gaps";
-            extraPackages = with pkgs; [ dmenu i3status i3lock ];
+            extraPackages = [ pkgs.dmenu pkgs.i3status pkgs.i3lock ];
         };
     };
 }
