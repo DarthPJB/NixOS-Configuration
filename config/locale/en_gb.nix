f51d109e9c0558f9a8fa6871d2d47ac2592cb8e9
John Bargman <darthpjb@gmail.com>
2020-12-06 21:32:40 +0000

    added locale and updated i3 implementation to be more compliant.

---
+++ config/locale/en_gb.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }: 
+
+{  
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+ #  Select internationalisation properties.
+   i18n.defaultLocale = "en_GB.UTF-8";
+   console = {
+     font = "Lat2-Terminus16";
+     keyMap = "uk";
+   };
+
+  # Configure keymap in X11
+  services.xserver.layout = "gb";
+}
