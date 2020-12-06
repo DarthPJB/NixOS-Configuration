51a199c05117c047b4a7a3cae6c9019db4a809d5
John Bargman <darthpjb@gmail.com>
2020-12-06 21:15:01 +0000

    and another semicolon

--- config/enviroments/i3wm.nix
+++ config/enviroments/i3wm.nix
@@ -13,7 +13,7 @@
         {
             enable = true;
             package = "pkgs.i3-gaps";
-            extraPackages = with pkgs; [ dmenu i3status i3lock ]
+            extraPackages = with pkgs; [ dmenu i3status i3lock ];
             config = rec 
             {
                 gaps = 
