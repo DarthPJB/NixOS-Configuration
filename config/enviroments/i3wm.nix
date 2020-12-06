87f2a52762681fdd64da560e9adc33ad31fa16ad
John Bargman <darthpjb@gmail.com>
2020-12-06 21:20:45 +0000

    removed i3-config (copy paste is bad ya'll)

--- config/enviroments/i3wm.nix
+++ config/enviroments/i3wm.nix
@@ -14,14 +14,6 @@
             enable = true;
             package = "pkgs.i3-gaps";
             extraPackages = with pkgs; [ dmenu i3status i3lock ];
-            config = rec 
-            {
-                gaps = 
-                {
-                    inner = 20;
-                    outer = 10;
-                };
-            };
         };
     };
 }
