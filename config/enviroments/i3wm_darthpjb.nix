d80c50e901b233556e031285ce0e01cf11743602
John Bargman <darthpjb@gmail.com>
2020-12-08 04:08:54 +0000

    updated dotfile and added some images for later use in UI

--- config/enviroments/i3wm_darthpjb.nix
+++ config/enviroments/i3wm_darthpjb.nix
@@ -5,5 +5,8 @@
     [ # Include the results of the hardware scan.
         ./i3wm.nix
     ];
-       environment.systemPackages = [pkgs.cool-retro-term pkgs.conky];
+    programs.dconf.enable = true;
+    services.gnome3.gnome-keyring.enable = true;
+    services.gnome3.seahorse.enable = true;
+    environment.systemPackages = [pkgs.cool-retro-term pkgs.conky pkgs.lxappearance];
 }
