28ab0a1fcf797f81e77b6a9f4c33dac24a850c37
john bargman <darthpjb@gmail.com>
2020-12-22 05:38:52 +0000

    Updated config to attempt gpu passthrough

--- config/enviroments/i3wm_darthpjb.nix
+++ config/enviroments/i3wm_darthpjb.nix
@@ -1,18 +1,24 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 
 {
     imports =
     [ # Include the results of the hardware scan.
         ./i3wm.nix
     ];
+    services.compton =
+    {
+      enable = true;
+      shadow = true;
+      inactiveOpacity = 0.6;
+      activeOpacity = 0.9;
+    };
     programs.dconf.enable = true;
     services.gnome3.gnome-keyring.enable = true;
     programs.seahorse.enable = true;
     environment.systemPackages = [
-	pkgs.cool-retro-term 
-	pkgs.conky 
+	pkgs.cool-retro-term
+	pkgs.conky
 	pkgs.lxappearance
-	pkgs.pulseaudio-ctl
 	pkgs.arandr
 	pkgs.firefox];
 }
--- config/machines/megajohn.nix
+++ config/machines/megajohn.nix
@@ -2,29 +2,43 @@
 {
 
   # Use the GRUB 2 boot loader.
-  boot.loader = {
-	grub = {
-	    enable = true;
-	    version = 2;
-	    configurationLimit = 5;
-	    efiSupport = true;
-	    efiInstallAsRemovable = true;
-	    device = "nodev"; # or "nodev" for efi only
-	  };
-        systemd-boot = {
-	    enable = true;
-	    };
+  boot = {
+    loader = {
+	     grub = {
+	        enable = true;
+	         version = 2;
+	          configurationLimit = 5;
+	           efiSupport = true;
+	            efiInstallAsRemovable = true;
+	             device = "nodev"; # or "nodev" for efi only
+	            };
+              systemd-boot = {
+	               enable = true;
+              };
+        };
+      kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+      blacklistedKernelModules = ["nouveau" "nvidia"];
+      kernelParams = ["intel_iommu=on"];
+      extraModprobeConfig = "options vfio-pci ids=10de:1401,8086:1912";
+#        postBootCommands = ''
+#        DEVS="0000:0f:00.0 0000:0f:00.1"
+
+#        for DEV in $DEVS; do
+#          echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+#        done
+#        modprobe -i vfio-pci
+#     '';
 	};
 
-  # Networking 
+  # Networking
   networking = {
     hostName = "megajohn"; # Define your hostname.
     interfaces = {
-	enp0s31f6.useDHCP = true;
-	wlp4s0.useDHCP = true;
+	     enp0s31f6.useDHCP = true;
+	      wlp4s0.useDHCP = true;
     };
   };
-  
+
   hardware = {
     opengl.enable = true;
     pulseaudio.enable = true;
@@ -39,11 +53,11 @@
         # Enable the OpenSSH daemon.
         openssh.enable = true;
         # Enable touchpad support (enabled default in most desktopManager).
-        xserver = { 
+        xserver = {
             # TODO: update this with appropriate entries
-            #displayManager.setupCommands = 
-	    digimend.enable = true;
-            videoDrivers = [ "nvida" ];
+            #displayManager.setupCommands =
+	           digimend.enable = true;
+            #videoDrivers = [ "nvida" ];
         };
         # Enable CUPS to print documents.
         printing.enable = true;
