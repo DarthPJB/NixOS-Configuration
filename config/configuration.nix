7ce064f97b3a2bf064660cfc15ad7769ae6d68fd
John Bargman <darthpjb@gmail.com>
2021-01-30 15:27:07 +0000

    Added Branch for TerminalZero

--- config/configuration.nix
+++ config/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./machines/megajohn.nix
+      ./machines/TerminalZero.nix
     ];
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
