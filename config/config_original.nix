a29af6c6c66b430f514034422b1578762c593500
John Bargman <darthpjb@gmail.com>
2021-08-01 23:06:52 +0100

    updated terminalZero with main branch updates2

--- config/config_original.nix
+++
@@ -1,8 +0,0 @@
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
