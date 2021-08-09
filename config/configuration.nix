7dfa6390a01f61dcf38b7c31b86598f434bada76
John Bargman <darthpjb@gmail.com>
2021-08-09 23:56:10 +0200

    testing flake usage

--- config/configuration.nix
+++ config/configuration.nix
@@ -8,37 +8,126 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./machines/megajohn.nix
     ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+   time.timeZone = "Europe/Amsterdam";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s25.useDHCP = true;
+  networking.interfaces.wlp3s0.useDHCP = true;
+  networking.interfaces.wwp0s29u1u4i6.useDHCP = true;
 
-  networking =
-  {
-    useDHCP = false;
-    networkmanager = {
-      enable = true;  # Enables wireless support via wpa_supplicant.
-      wifi.backend = "wpa_supplicant";
-      dhcp = "dhclient";
-    };
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+#   i18n.defaultLocale = "en_UK.UTF-8";
+   console = {
+     font = "Lat2-Terminus16";
+     keyMap = "uk";
+   };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+ #  services.xserver.windowManager.i3.enable = true;
+
+  hardware.opengl.enable = true;
+ #programs.sway.enable = true;
+  programs.sway = {
+  enable = true;
+  wrapperFeatures.gtk = true; # so that gtk works properly
+  extraPackages = with pkgs; [
+    swaylock
+    swayidle
+    wl-clipboard
+    mako # notification daemon
+    alacritty # Alacritty is the default terminal in the config
+    dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
+  ];
+};
+
+
+  # Configure keymap in X11
+  services.xserver.layout = "gb";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.darthpjb = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "video" "dailout" ]; # Enable ‘sudo’ for the user.
+     password= "godlet";
   };
 
-  # List packages installed in system profile. To search, run:
+ #  List packages installed in system profile. To search, run:
   # $ nix search wget
-   environment.systemPackages = [
-     pkgs.wget
-     pkgs.nano
-     pkgs.git
-     pkgs.mkpasswd
-     pkgs.htop
-     pkgs.vim
-     pkgs.tmux
-     pkgs.pciutils
-     pkgs.ranger
+   environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     firefox
+     brightnessctl
+     atom
+     alacritty
    ];
 
-  system.stateVersion = "20.09"; # Did you read the comment?
+   nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+   };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+   programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
 
 }
