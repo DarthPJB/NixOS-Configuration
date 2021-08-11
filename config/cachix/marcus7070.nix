58482aff86ebe6c76db2ffa6925109e7e67c7f85
John Bargman <darthpjb@gmail.com>
2021-08-11 15:48:04 +0200

    added cachix to avoid compiling cadquery, ever again.

---
+++ config/cachix/marcus7070.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://marcus7070.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "marcus7070.cachix.org-1:JawxHSgnYsgNYJmNqZwvLjI4NcOwrcEZDToWlT3WwXw="
+    ];
+  };
+}
