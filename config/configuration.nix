1a79b5ca5cd7bc60e7395577ced69b475af5bb25
John Bargman <darthpjb@gmail.com>
2021-08-27 00:33:21 +0100

    added wifi support

--- config/configuration.nix
+++ config/configuration.nix
@@ -21,9 +21,14 @@
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-   networking.hostName = "terminalzero"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+  networking.hostName = "terminalzero"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.userControlled.enable = true;
+  networking.wireless.networks = {
+    "Astral_Ship" = {
+      pskRaw = "ff866b7b9494bd6915c28a06c8604d1e2396e590e64f71b2fdf9c0c9709ec2c4";
+  };
+};
   # Set your time zone.
    time.timeZone = "Europe/London";
 
