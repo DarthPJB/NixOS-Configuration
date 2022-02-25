3700d7739bf10726baf133a8576bac785c36672c
John Bargman <darthpjb@gmail.com>
2022-02-25 19:24:11 +0000

    updating imports to reflect flake nixpkgs

--- config/modifier_imports/unstable_nixpkgs.nix
+++ config/modifier_imports/unstable_nixpkgs.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }:
-{
-  baseconfig = { allowUnfree = true; };
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-  unstable = import unstableTarball
-  {
-    config = baseconfig;
-  };
-}
+#{ config, pkgs, ... }:
+#{
+#  baseconfig = { allowUnfree = true; };
+#  unstableTarball =
+#    fetchTarball
+#      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+#  unstable = import unstableTarball
+#  {
+#    config = baseconfig;
+#  };
+#}
