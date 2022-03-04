f103e907bbc8f022b7b946f2e9d2811659180d19
John Bargman <darthpjb@gmail.com>
2022-03-04 19:02:45 +0000

    updated background script with support for three monitors

--- winwrap.sh
+++ winwrap.sh
@@ -4,4 +4,10 @@ if [ `DISPLAY=:0 xrandr -q | grep ' connected' | wc -l` = "2" ]
 then
 	xwinwrap -ov -b -nf -o 1.0 -g 1920x1080+1920+0 -debug -- mplayer -wid WID -loop 0 ~/ascetics_bin/video/gobi_stars.mp4 &
 fi
+if [ `DISPLAY=:0 xrandr -q | grep ' connected' | wc -l` = "3" ]
+then
+	xwinwrap -ov -b -nf -o 1.0 -g 1080x1920 -debug -- mplayer -wid WID -loop 0  ~/ascetics_bin/video/gobi_stars.mp4 &
+	xwinwrap -ov -b -nf -o 1.0 -g 1920x1080+1080+0 -debug -- mplayer -wid WID -loop 0  ~/ascetics_bin/video/gobi_stars.mp4 && fg
+else
 	xwinwrap -ov -b -nf -o 1.0 -g 1920x1080 -debug -- mplayer -wid WID -loop 0  ~/ascetics_bin/video/gobi_stars.mp4 && fg
+fi
