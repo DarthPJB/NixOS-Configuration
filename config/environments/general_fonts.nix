ffca143e1d24b478cccf5a394325c737881bf48d
John Bargman <darthpjb@gmail.com>
2022-04-03 03:59:23 +0100

    removed excess fonts

--- config/environments/general_fonts.nix
+++ config/environments/general_fonts.nix
@@ -3,13 +3,13 @@
 {
   fonts.fonts = with pkgs; [
     noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
+    #noto-fonts-cjk
+    #noto-fonts-emoji
     liberation_ttf
     fira-code
-    fira-code-symbols
-    mplus-outline-fonts
-    dina-font
-    proggyfonts
+    #fira-code-symbols
+    #mplus-outline-fonts
+    #dina-font
+    #proggyfonts
   ];
 }
