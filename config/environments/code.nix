9dd4fa28e1d6722b3a78aa42f429922b419e982c
John Bargman <darthpjb@gmail.com>
2022-04-28 06:33:30 +0100

    splitting neovim into it's own config - but can't get plugins working yet

--- config/environments/code.nix
+++ config/environments/code.nix
@@ -4,14 +4,11 @@
 environment.systemPackages = with pkgs; [
 	pkgs.gpp
 	pkgs.emscripten
-#	pkgs.emscriptenStdenv
-#	pkgs.emscriptenPackages.zlib
 	pkgs.atom
 	pkgs.vscode
 	pkgs.neovim
 	pkgs.cool-retro-term
 	pkgs.terminator
 	pkgs.enlightenment.terminology
-	pkgs.neovim
 ];
 }
--- flake.nix
+++ flake.nix
@@ -57,6 +57,7 @@
           (import ./config/environments/bluetooth.nix)
           (import ./config/environments/steam.nix)
           (import ./config/environments/code.nix)
+          (import ./config/environments/neovim.nix)
           (import ./config/environments/cad_and_graphics.nix)
           (import ./config/environments/audio_visual_editing.nix)
           (import ./config/environments/general_fonts.nix)
