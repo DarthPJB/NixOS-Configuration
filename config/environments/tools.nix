e0f380c4febbe912b0050853f128395499f5cc52
John Bargman <darthpjb@gmail.com>
2022-06-29 22:20:59 +0100

    updated for new setup at mr.bornbastic's

--- config/environments/tools.nix
+++ config/environments/tools.nix
@@ -2,14 +2,16 @@
 
 {
   environment.systemPackages = [
-  pkgs.btop
-  pkgs.nano
-  pkgs.wget
-  pkgs.git
-  pkgs.ranger
-  pkgs.killall
-  pkgs.magic-wormhole
-  pkgs.wpa_supplicant_gui
-  pkgs.freemind
-  ];
+        pkgs.btop
+        pkgs.nano
+        pkgs.wget
+        pkgs.git
+        pkgs.ranger
+        pkgs.killall
+        pkgs.magic-wormhole
+        pkgs.wpa_supplicant_gui
+        pkgs.freemind
+        pkgs.dnsutils
+        pkgs.pciutils
+    ];
 }
