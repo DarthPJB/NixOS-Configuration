377ce39735e9d482c960436e4776c887733885c4
John Bargman <darthpjb@gmail.com>
2022-07-07 18:13:02 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    parsecgaming.url = "github:DarthPJB/parsec-gaming-nix";
+    #parsecgaming.url = "github:DarthPJB/parsec-gaming-nix";
     nixos-hardware.url = "github:nixos/nixos-hardware";
   };
 
