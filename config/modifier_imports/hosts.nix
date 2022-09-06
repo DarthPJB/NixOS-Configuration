66d8e7ca3c4fa821ee749b702fd2a191085d4fa4
John Bargman <darthpjb@gmail.com>
2022-09-06 19:56:45 +0100

    updated hosts to provide platonic internals

--- config/modifier_imports/hosts.nix
+++ config/modifier_imports/hosts.nix
@@ -5,5 +5,7 @@
         144.126.215.24 mattermoist.platonic
         193.16.42.101 remote.worker
         193.16.42.125 ark.server
+	216.24.131.6 acropolis.platonic
+	216.24.131.5 tumulus.platonic
     '';
 }
