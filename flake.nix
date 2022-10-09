d481fabf4db8a3999472b7f2e9e79809999bf7ee
John Bargman <darthpjb@gmail.com>
2022-10-09 05:12:07 +0100

    updated winwrap and added communications

--- flake.nix
+++ flake.nix
@@ -88,6 +88,7 @@
           (import ./config/environments/i3wm_darthpjb.nix)
           (import ./config/environments/steam.nix)
           (import ./config/environments/code.nix)
+          (import ./config/environments/communications.nix)
           (import ./config/environments/neovim.nix)
           (import ./config/environments/cad_and_graphics.nix)
           (import ./config/environments/3dPrinting.nix)
