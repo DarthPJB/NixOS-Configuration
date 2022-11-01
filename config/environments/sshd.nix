39abc0ab7abe76e93d2350ca22e509b557381781
John Bargman <darthpjb@gmail.com>
2022-11-01 11:48:02 +0000

    corrected ssh ports and began configuration for nextcloud

--- config/environments/sshd.nix
+++ config/environments/sshd.nix
@@ -3,9 +3,9 @@
 {
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.openssh.ports = [ 1108 22 ];
+  services.openssh.ports = [ 1108 ];
   services.openssh.passwordAuthentication = false;
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 1108 22 ];
+  networking.firewall.allowedTCPPorts = [ 1108 ];
   networking.firewall.allowedUDPPorts = [];
 }
