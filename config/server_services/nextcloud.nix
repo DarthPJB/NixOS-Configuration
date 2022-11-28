e45f28fa30d5c7c8a21e65d1cc8587534287c0a9
John Bargman <darthpjb@gmail.com>
2022-11-28 13:01:28 +0000

    updated config to allow for larger uploads by adjusting timeout

--- config/server_services/nextcloud.nix
+++ config/server_services/nextcloud.nix
@@ -19,7 +19,6 @@ in
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
-  
   services.nextcloud = 
   {
     enable = true;                   
@@ -40,5 +39,13 @@ in
         secretFile = config.age.secrets.nextcloud_s3_key.path;
       };
     };
+
   };
+
+  services.nginx.virtualHosts."nextcloud.johnbargman.com" = {
+    extraConfig = "fastcgi_read_timeout 86400;\n";
+  };
+  services.phpfpm.pools.nextcloud = {
+    phpOptions = "php_admin_value[max_input_time] = 86400\nphp_admin_value[max_execution_time] = 86400\n";
+  }; 
 }
