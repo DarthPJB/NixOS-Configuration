fc59851fd5089ec7069c7aa1b678cda9f2f5be26
John Bargman <darthpjb@gmail.com>
2022-12-14 19:29:52 +0000

    updated hosts

--- config/modifier_imports/hosts.nix
+++ config/modifier_imports/hosts.nix
@@ -2,11 +2,11 @@
 {
     networking.extraHosts =
     ''
-        144.126.215.24 mattermoist.platonic
         193.16.42.101 remote.worker
         193.16.42.125 ark.server
 	216.24.131.6 acropolis.platonic
         216.24.131.5 tumulus.platonic
+        216.24.131.4 springboard.platonic
         193.16.42.101 nextcloud.johnbargman.com
         192.168.1.104 nasa.astral
     '';
