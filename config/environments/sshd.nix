829df23e20ac1ddef98edbbb0203971c7e575738
John Bargman <darthpjb@gmail.com>
2023-01-09 14:21:35 +0000

    Update sshd.nix
    protect against root login

--- config/environments/sshd.nix
+++ config/environments/sshd.nix
@@ -4,6 +4,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.ports = [ 1108 ];
+  services.openssh.permitRootLogin = "no";
   services.openssh.passwordAuthentication = false;
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 1108 ];
