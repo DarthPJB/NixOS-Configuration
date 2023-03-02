5eaaefe2f7fcd84835966c5b84261efe352d318f
John Bargman <darthpjb@gmail.com>
2023-03-02 04:33:48 +0000

    modified configuration to use native arm7

--- config/machines/rPI.nix
+++ config/machines/rPI.nix
@@ -1,41 +1,18 @@
 { pkgs, config, lib, ... }:
 {
   boot = {
-    # Use mainline kernel
-    loader.raspberryPi = {
-        enable = true;
-        version = 3;
-        firmwareConfig = ''
-          core_freq=250
-        '';
-    };
-    kernelParams = [ "console=ttyS1,115200n8" ];
-    initrd.kernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
-    kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = lib.mkForce [ "bridge" "macvlan" "tap" "tun" "loop" "atkbd" "ctr" ];
-    supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "ext4" "vfat" ];
+
+    # Cleanup tmp on startup
+    cleanTmpDir = true;
+    kernelParams = [ "console=ttyS1,115200n8" "cma=32M" ];
+    #initrd.kernelModules = [ "vc4" "bcm2835_dma" "i2c_bcm2835" ];
   };
-  # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix" usually contains this
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-    };
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
 
-  boot.loader.raspberryPi.firmwareConfig = ''
-    dtparam=audio=on
-
-  environment.systemPackages = with pkgs; [ vim git ];
+  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+    
+  system.stateVersion = "22.05";
   services.openssh.enable = true;
   networking.hostName = "pi";
-  users = {
-    users.myUsername = {
-      password = "myPassword";
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-    };
-  };
   networking = {
     interfaces."wlan0".useDHCP = true;
     wireless = {
