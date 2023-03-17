e13450cc585cc34f66ba6749d2f53c65a44f2cee
John Bargman <darthpjb@gmail.com>
2023-03-17 20:11:20 +0000

    br0 prevented boot

--- config/modifier_imports/virtualisation-libvirtd.nix
+++ config/modifier_imports/virtualisation-libvirtd.nix
@@ -9,16 +9,16 @@
       };
    };
    users.extraGroups.libvirtd.members = [ "John88" ];
-   boot.kernel.sysctl = { "net.ipv4.ip_forward" = 1; };
+   #boot.kernel.sysctl = { "net.ipv4.ip_forward" = 1; };
 
 #   networking.bridges.br0.interfaces = ["enp69s0f0"];
-   programs.dconf.enable = true;
+   #programs.dconf.enable = true;
    environment.systemPackages = with pkgs; [ virt-manager ];
-   networking.interfaces.br0 = {
-    useDHCP = false;
-    ipv4.addresses = [{
-      "address" = "10.0.0.5";
-      "prefixLength" = 24;
-    }];
-  };
+   #networking.interfaces.br0 = {
+#    useDHCP = false;
+    #ipv4.addresses = [{
+      #"address" = "10.0.0.5";
+      #"prefixLength" = 24;
+    #}];
+  #};
 }
