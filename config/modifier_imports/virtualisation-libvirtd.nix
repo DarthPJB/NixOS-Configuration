6fa3b0f4f0dd13748ac767d004efc21fdf74a91f
John Bargman <darthpjb@gmail.com>
2023-03-17 01:30:13 +0000

    added libvirtd

---
+++ config/modifier_imports/virtualisation-libvirtd.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+{
+   virtualisation = 
+   {
+      libvirtd = 
+      {
+         enable = true;
+         qemu.ovmf.enable = true;
+      };
+   };
+   users.extraGroups.libvirtd.members = [ "John88" ];
+   boot.kernel.sysctl = { "net.ipv4.ip_forward" = 1; };
+
+#   networking.bridges.br0.interfaces = ["enp69s0f0"];
+   programs.dconf.enable = true;
+   environment.systemPackages = with pkgs; [ virt-manager ];
+   networking.interfaces.br0 = {
+    useDHCP = false;
+    ipv4.addresses = [{
+      "address" = "10.0.0.5";
+      "prefixLength" = 24;
+    }];
+  };
+}
