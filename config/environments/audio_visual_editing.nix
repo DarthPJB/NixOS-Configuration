851d9f77ef571e87620000c5ebcbc16ad5caac37
john bargman <darthpjb@gmail.com>
2023-06-20 22:45:51 +0100

    fixed and working

--- config/environments/audio_visual_editing.nix
+++ config/environments/audio_visual_editing.nix
@@ -1,7 +1,17 @@
 { config, pkgs, inputs, ... }:
 
 {
-	environment.systemPackages = [
+	environment.systemPackages = 
+	let
+		system = "x86_64-linux";
+		pkgs_unstable = import inputs.nixpkgs_unstable {
+			inherit system;
+			config.allowUnfree = true; 
+		};
+
+		#pkgs_unstable = inputs.nixpkgs_unstable.legacyPackages.x86_64-linux;
+	in [
+		# blender
 		pkgs_unstable.ffmpeg-full
 		pkgs.mplayer
 		pkgs.vlc
--- config/environments/blender.nix
+++ config/environments/blender.nix
@@ -1,6 +1,15 @@
 { config, pkgs, inputs, ... }:
 {
-	environment.systemPackages = [
+	environment.systemPackages = 
+	let
+		system = "x86_64-linux";
+		pkgs_unstable = import inputs.nixpkgs_unstable {
+			inherit system;
+			config.allowUnfree = true; 
+		};
+
+		#pkgs_unstable = inputs.nixpkgs_unstable.legacyPackages.x86_64-linux;
+	in [
 		(pkgs_unstable.blender.override { cudaSupport = true; })
 	];
 }
