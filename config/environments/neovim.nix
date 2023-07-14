10b6a643520b48fdcf5da1ff66d2b21a379c9d44
john bargman <darthpjb@gmail.com>
2023-07-14 04:12:13 +0100

    configuration now builds as expected

--- config/environments/neovim.nix
+++ config/environments/neovim.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs;
     let
-        myNvim = inputs.nixpkgs_2205.legacyPackages.x86_64-linux.neovim.override {
+        myNvim = inputs.nixpkgs.legacyPackages.x86_64-linux.neovim.override {
           vimAlias = true;
           configure = (import ./chloe/nvim { inherit pkgs; });
         };
