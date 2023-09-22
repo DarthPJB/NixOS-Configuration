8d27e23a948d30b8eab83c518ad0205470362e32
john bargman <darthpjb@gmail.com>
2023-09-22 02:04:13 +0100

    making computer go

--- config/machines/obs-box.nix
+++ config/machines/obs-box.nix
@@ -3,14 +3,76 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, lib, ... }:
-
 {
+  systemd.user.services.upDeck =
+    let
+      upDeck = builtins.fetchurl {
+        url = "https://8up.uk/downloads/UPDeck_2-1-19.love";
+        sha256 = "1m8s83hdmkxym2frcwbzhibkxa864dpsbpd2jsmn3gk2sc8xid89";
+      };
+    in
+    {
+      description = "upDeck";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      serviceConfig =
+        {
+          ExecStart = ''
+            ${pkgs.love}/bin/love ${upDeck}
+          '';
+          #${pkgs.obs-studio}/bin/obs
+          #${pkgs.coreutils-full}/bin/echo display: $DISPLAY
+          #echo display is $DISPLAY
+          #echo authority is $XAUTHORITY
+          # 
+          #${pkgs.x11vnc}/bin/x11vnc -ncache 10 -display :0 &
+          PassEnvironment = "DISPLAY XAUTHORITY";
+        };
+    };
+  systemd.user.services.obs-auto =
+    {
+      description = "obs-studio-autostart";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      serviceConfig =
+        {
+          ExecStart = ''
+            ${pkgs.obs-studio}/bin/obs
+          '';
+          PassEnvironment = "DISPLAY XAUTHORITY";
+        };
+    };
+  systemd.user.services.x11vnc =
+    {
+      description = "run X11 vnc server";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      serviceConfig =
+        {
+          ExecStart = ''
+            ${pkgs.x11vnc}/bin/x11vnc -ncache 10 -display $DISPLAY 
+          '';
+          PassEnvironment = "DISPLAY XAUTHORITY";
+        };
+    };
+
   imports =
     [
       # Include the results of the hardware scan.
       ./obs-box/hardware-configuration.nix
     ];
-
+  security = {
+    sudo = {
+      wheelNeedsPassword = false;
+      extraConfig = ''
+        %psudo ALL=(ALL) PASSWD: ALL
+      '';
+    };
+  };
+  environment.extraInit = ''
+    xset s off -dpms
+  '';
+  #services.xrdp.enable = true;
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -32,7 +94,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.ports = [ 1108 22 ];
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 1108 22 ];
+  networking.firewall.allowedTCPPorts = [ 1108 22 5901 ];
 
   # Configure keymap in X11
 
@@ -70,9 +132,14 @@
           libinput.enable = true;
           videoDrivers = [ "nvidia" ];
           layout = "gb";
-          #	deviceSection = ''
-          #	  Option "Coolbits" "24"
-          #	'';
+          displayManager = {
+            defaultSession = "none+i3";
+            autoLogin = {
+              enable = true;
+              user = "John88";
+            };
+          };
+          windowManager.i3.enable = true;
         };
     };
 }
