6da27236aac5012b937cd0168e076803507e3625
john bargman <darthpjb@gmail.com>
2023-12-07 13:56:44 +0000

    refactor config to top level

---
+++ modifier_imports/vnc-server.nix
@@ -0,0 +1,17 @@
+  { config, pkgs, ... }:
+{
+  
+  systemd.user.services.x11vnc =
+    {
+      description = "run X11 vnc server";
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig =
+        {
+          Restart="always";
+          ExecStart = ''
+            ${pkgs.x11vnc}/bin/x11vnc -display $DISPLAY
+          '';
+          PassEnvironment = "DISPLAY XAUTHORITY";
+        };
+    };
+}
