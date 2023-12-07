c439214877e3b2cb4071388fcc9125ef8606c6d1
john bargman <darthpjb@gmail.com>
2023-12-07 20:56:10 +0000

    time for some nas-mounts

--- environments/i3wm_darthpjb.nix
+++ environments/i3wm_darthpjb.nix
@@ -12,7 +12,7 @@
       wantedBy = [ "graphical-session.target" ];
       serviceConfig =
         {
-          Restart="always";
+          Restart = "always";
           ExecStart = ''
             ${pkgs.mumble}/bin/mumble
           '';
@@ -26,56 +26,56 @@
       wantedBy = [ "graphical-session.target" ];
       serviceConfig =
         {
-          Restart="always";
+          Restart = "always";
           ExecStart = ''
-             ${pkgs.xwinwrap}/bin/xwinwrap -ov -fs -- ${pkgs.xscreensaver}/libexec/xscreensaver/glmatrix -root -window-id WID
+            ${pkgs.xwinwrap}/bin/xwinwrap -ov -fs -- ${pkgs.xscreensaver}/libexec/xscreensaver/glmatrix -root -window-id WID
           '';
           PassEnvironment = "DISPLAY XAUTHORITY";
         };
     };
   services.picom =
     {
-        enable = true;
-		activeOpacity = 1;
-		inactiveOpacity = 0.96;
-		backend = "glx";
-		fade = true;
-		fadeDelta = 5;
+      enable = true;
+      activeOpacity = 1;
+      inactiveOpacity = 0.96;
+      backend = "glx";
+      fade = true;
+      fadeDelta = 5;
 
-		#you can get the CLASS_NAME of any window by executing the following command and clicking on a window.
-		#xprop | grep "CLASS"
-		#Note: The CLASS_NAME value is actually the second one.
+      #you can get the CLASS_NAME of any window by executing the following command and clicking on a window.
+      #xprop | grep "CLASS"
+      #Note: The CLASS_NAME value is actually the second one.
 
-		opacityRules = [
-            "100:class_g = 'looking-glass-client'"
-            "100:class_g = 'looking-glass-client' && focused"
-			"100:class_g = 'i3lock' && focused"
-			"100:class_g = 'i3lock' && !focused"
-			"100:class_g = 'i3lock-color' && focused"
-			"100:class_g = 'i3lock-color' && !focused"
-			"100:class_g = 'betterlockscreen' && focused"
-			"100:class_g = 'betterlockscreen' && !focused"
-			"80:class_g = 'i3bar'"
-			"80:class_g = 'Polybar'"
-			"100:class_g = 'firefox'"
-			"50:class_g = 'Alacritty' && focused"
-			"50:class_g = 'Alacritty' && !focused"
-			"100:class_g = 'Vivaldi-stable' && focused"
-			"100:class_g = 'Brave-browser' && focused"
-            "90:fullscreen"
-		];
-    	shadow = true;
-		shadowOpacity = 0.75;
-		settings = {
-			#blur = { 
-				#method = "gaussian";
-				#blur-strength = "10";
-				#size = 20;
-				#deviation = 5.0;
-				#blur-background = true;
-			#};
-		};
-	};
+      opacityRules = [
+        "100:class_g = 'looking-glass-client'"
+        "100:class_g = 'looking-glass-client' && focused"
+        "100:class_g = 'betterlockscreen' && focused"
+        "100:class_g = 'betterlockscreen' && !focused"
+        "80:class_g = 'i3bar'"
+        "80:class_g = 'Polybar'"
+        "100:class_g = 'firefox'"
+        "50:class_g = 'Alacritty' && focused"
+        "50:class_g = 'Alacritty' && !focused"
+        "100:class_g = 'Vivaldi-stable' && focused"
+        "100:class_g = 'Brave-browser' && focused"
+        "90:fullscreen"
+        "80:class_g = 'i3lock' && focused"
+        "80:class_g = 'i3lock' && !focused"
+        "80:class_g = 'i3lock-color' && focused"
+        "80:class_g = 'i3lock-color' && !focused"
+      ];
+      shadow = true;
+      shadowOpacity = 0.75;
+      settings = {
+        #blur = { 
+        #method = "gaussian";
+        #blur-strength = "10";
+        #size = 20;
+        #deviation = 5.0;
+        #blur-background = true;
+        #};
+      };
+    };
   programs.dconf.enable = true;
   environment.systemPackages =
     [
--- modifier_imports/vnc-server.nix
+++ modifier_imports/vnc-server.nix
@@ -1,13 +1,13 @@
-  { config, pkgs, ... }:
+{ config, pkgs, ... }:
 {
-  
+
   systemd.user.services.x11vnc =
     {
       description = "run X11 vnc server";
       wantedBy = [ "graphical-session.target" ];
       serviceConfig =
         {
-          Restart="always";
+          Restart = "always";
           ExecStart = ''
             ${pkgs.x11vnc}/bin/x11vnc -display $DISPLAY
           '';
