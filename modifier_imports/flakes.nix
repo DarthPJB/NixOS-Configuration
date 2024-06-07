bea3f528a71cf79988cc0631d092f8f962816172
john bargman <darthpjb@gmail.com>
2024-06-07 11:50:15 +0100

    formatting and flake lock

--- modifier_imports/flakes.nix
+++ modifier_imports/flakes.nix
@@ -2,7 +2,7 @@
 {
   nix = {
     settings.trusted-users = [ "root" "John88" ];
-    package = pkgs.nixUnstable;
+    package = pkgs.nixVersions.latest;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
