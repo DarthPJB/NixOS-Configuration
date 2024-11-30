94defcd294c4ddbb186677167582cba4d043e967
John Bargman <john.bargman@platonic.systems>
2024-11-30 10:45:16 +0000

    flaking around

--- modifier_imports/cuda.nix
+++ modifier_imports/cuda.nix
@@ -13,15 +13,15 @@
       pkgs_un = self.un_pkgs;
     in
     [
-#      pkgs.nvtopPackages.full
-#      pkgs.cudaPackages.cudatoolkit
-#      pkgs.cudaPackages.cudnn
- #     pkgs.cudaPackages.cutensor
+      pkgs.nvtopPackages.full
+      pkgs.cudaPackages.cudatoolkit
+      pkgs.cudaPackages.cudnn
+#     pkgs.cudaPackages.cutensor
       #      pkgs.ollama
       #      (pkgs.llama-cpp.override { cudaSupport = true; })
 
-      #      (pkgs.colmap.override { cudaSupport = true; })
-#      (pkgs.blender.override { cudaSupport = true; })
+      (pkgs.colmap.override { cudaSupport = true; })
+      (pkgs.blender.override { cudaSupport = true; })
     ];
 
   nixpkgs.config = {
