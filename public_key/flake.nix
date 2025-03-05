ad8cabd8dc8fb7994d01d567539c82f5195bf804
John Bargman <john.bargman@platonic.systems>
2025-03-05 01:35:24 +0000

    add some more public key data

---
+++ public_key/flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs.keyFlake.url = "github:pinktrink/keyFlake.nix";
+  outputs = { self, keyFlake, ... }: let
+    inherit (keyFlake.lib) mkKeyFlake;
+  in mkKeyFlake {
+    myHostName = {
+      users.david = [  (builtins.readFile ./id_ed25519_master.pub)  ];
+    };
+  };
+}
