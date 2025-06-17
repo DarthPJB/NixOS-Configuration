50558ad017ec784f68e402bd73634fffe1f6e7c4
John Bargman <john.bargman@platonic.systems>
2025-06-17 23:56:36 +0000

    add github builder and prepare secret

--- machines/openstack.nix
+++ machines/openstack.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
-  system.stateVersion = "22.11";
   services.openssh.ports = [ 22 ];
   networking.firewall.allowedTCPPorts = [ 22 ];
 }
--- services/dynamic_domain_gandi.nix
+++ services/dynamic_domain_gandi.nix
@@ -15,18 +15,16 @@ in
   secrix.services."dynamic-${hostname}".secrets.gandi_api_barg_net_token.encrypted.file = "${self}/secrets/gandi_api_barg_net_token";
   systemd.services."dynamic-${hostname}" =
     {
+      environment = {
+        DOMAIN = hostname;  # Domain hosted with Gandi
+        SUBDOMAIN = config.networking.hostName;  # Subdomain to update DNS
+      };
       script = ''
         # This script gets the external IP of your systems then connects to the Gandi
         # LiveDNS API and updates your dns record with the IP.
 
         # Gandi LiveDNS API KEY
         API_KEY=$(${pkgs.coreutils}/bin/cat ${config.secrix.services."dynamic-${hostname}".secrets.gandi_api_barg_net_token.decrypted.path})
-
-        # Domain hosted with Gandi
-        DOMAIN="${hostname}"
-
-        # Subdomain to update DNS
-        SUBDOMAIN="${config.networking.hostName}"
 
         # Get external IP address
         EXT_IP=$(${pkgs.lib.getExe pkgs.curl} -s ifconfig.me)  
@@ -43,11 +41,6 @@ in
                     \"rrset_values\": [\"$EXT_IP\"]}" \
                 $CURRENT_ZONE_HREF/$SUBDOMAIN/A
       '';
-      serviceConfig = {
-        Type = "oneshot";
-        User = "root";
-      };
-
+      serviceConfig.Type = "oneshot";
     };
 }
-
