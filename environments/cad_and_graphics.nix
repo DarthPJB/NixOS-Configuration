119cd18ebff8bd010c427c7e6a69c59cd70e43ab
John Bargman <john.bargman@platonic.systems>
2025-06-22 04:38:08 +0000

    add cortex-alpha

--- environments/cad_and_graphics.nix
+++ environments/cad_and_graphics.nix
@@ -11,12 +11,12 @@
       pkgs.krita
     ];
 
-  services.monado = {
-    enable = true;
-    defaultRuntime = true; # Register as default OpenXR runtime
-  };
-  systemd.user.services.monado.environment = {
-    STEAMVR_LH_ENABLE = "1";
-    XRT_COMPOSITOR_COMPUTE = "1";
-  };
+#  services.monado = {
+#    enable = true;
+#    defaultRuntime = true; # Register as default OpenXR runtime
+#  };
+#  systemd.user.services.monado.environment = {
+#    STEAMVR_LH_ENABLE = "1";
+#    XRT_COMPOSITOR_COMPUTE = "1";
+#  };
 }
---
+++ machines/cortex-alpha/default.nix
@@ -0,0 +1,70 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
+  networking.hostName = "cortex-alpha"; # Define your hostname.
+  # Set your time zone.
+  time.timeZone = "Etc/UTC";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.deploy = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       tree
+       neovim
+     ];
+   };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
