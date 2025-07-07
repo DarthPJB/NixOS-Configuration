97c426e1262c2a00f0ab0c61461147b7a5c3be36
John Bargman <john.bargman@platonic.systems>
2025-07-07 20:13:14 +0000

    protoDHCP

--- machines/cortex-alpha/hardware-configuration.nix
+++ machines/cortex-alpha/hardware-configuration.nix
@@ -47,15 +47,8 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   #networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp3s0 = {
-   useDHCP = lib.mkDefault false;
-     ipv4.addresses = [{ 
-   	address = "10.88.128.2"; prefixLength = 24;
-     }];
-   };
-  networking.interfaces.enp2s0 = {
-    useDHCP = lib.mkDefault false;
-  };
+  
+
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
