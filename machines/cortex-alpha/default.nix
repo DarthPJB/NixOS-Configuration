d340dc906b89f308034f53ad3744f7d74eff5823
John Bargman <john.bargman@platonic.systems>
2025-07-12 03:15:33 +0000

    from a single thread may a connection grow

--- machines/cortex-alpha/default.nix
+++ machines/cortex-alpha/default.nix
@@ -21,9 +21,45 @@
   };
   # Set your time zone.
   time.timeZone = "Etc/UTC";
-
+  secrix.services.wireguard-wireg0.secrets.cortex-alpha.encrypted.file = ../../secrets/wg_cortex-alpha;
   networking = {
-    wireguard.enable = true;
+    wireguard = { 
+      enable = true;
+      interfaces = {
+        wireg0 = 
+        {
+          # Determines the IP address and subnet of the server's end of the tunnel interface.
+          ips = [ "10.88.127.1/24" ];
+
+          # The port that WireGuard listens to. Must be accessible by the client.
+          listenPort = 2108;
+
+          # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+          # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+         /* postSetup = ''
+            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.88.127.0/24 -o enp2s0 -j MASQUERADE
+          '';
+
+          # This undoes the above command
+          postShutdown = ''
+            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.88.127.0/24 -o enp2s0 -j MASQUERADE
+          '';*/
+
+          # Path to the private key file.
+          privateKeyFile = config.secrix.services.wireguard-wireg0.secrets.cortex-alpha.decrypted.path;
+
+          peers = [
+            # List of allowed peers.
+            { # Feel free to give a meaningful name
+              # Public key of the peer (not a file path).
+              publicKey = "./secrets/wg_LINDA_pub";
+              # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+              allowedIPs = [ "10.88.127.88/32" ];
+            }
+          ];
+        };
+      };
+    };
     hostName = "cortex-alpha"; # Define your hostname.
     hostId = "c043a1fa";
     interfaces.enp3s0 = {
@@ -40,7 +76,7 @@
 
     };
     firewall.interfaces = {
-      "enp2s0".allowedUDPPorts = [ 1108 ];
+      "enp2s0".allowedUDPPorts = [ 1108 2108 ];
       "enp3s0".allowedUDPPorts = [ 67 /* DHCP */ 53 /*dns*/ ];
     };
     nat = {
@@ -53,12 +89,6 @@
   };
   services.dnsmasq = {
     enable = true;
-    servers = [
-      "208.67.220.220"
-      "208.67.222.222"
-      "1.0.0.1"
-      "8.8.8.8"
-    ];
     settings = {
       # upstream DNS servers
       # sensible behaviours
@@ -67,7 +97,12 @@
       no-resolv = true;
       # Cache dns queries.
       cache-size = 1000;
-
+      server = [
+        "208.67.220.220"
+        "208.67.222.222"
+        "1.0.0.1"
+        "8.8.8.8"
+      ];
       dhcp-range = [ "enp3s0,10.88.128.50,10.88.128.254,24h" ];
       interface = "enp3s0";
       dhcp-host = "10.88.128.1";
