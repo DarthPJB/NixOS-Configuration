ef75a4e0ae26b11815d8eb2944de2b822ce2fdc7
John Bargman <john.bargman@platonic.systems>
2025-07-29 00:48:28 +0000

    refine and improve vpn

--- machines/remote-worker/default.nix
+++ machines/remote-worker/default.nix
@@ -5,7 +5,15 @@
 {
   imports =
     [
-    ];
+    ../../lib/enable-wg.nix
+  ];
+  secrix.services.wireguard-wireg0.secrets.remote-worker.encrypted.file = ../../secrets/wg_remote-worker;
+  environment.vpn =
+    {
+      enable = true;
+      postfix = 50;
+      privateKeyFile = config.secrix.services.wireguard-wireg0.secrets.remote-worker.decrypted.path;
+    };
 
   networking.hostId = "e3fabb5b";
   networking.hostName = "remote-worker";
