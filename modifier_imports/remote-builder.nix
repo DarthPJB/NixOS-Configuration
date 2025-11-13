f4d95da5112d22cd604e285051639c14f0f541a1
John Bargman <john.bargman@platonic.systems>
2025-11-13 20:55:09 +0000

    disable distributed builders

--- modifier_imports/remote-builder.nix
+++ modifier_imports/remote-builder.nix
@@ -1,22 +1,44 @@
 { config, pkgs, ... }:
 
 {
-  nix.buildMachines = [{
-    hostName = "100.107.101.14";
-    system = "x86_64-linux";
-    protocol = "ssh-ng";
-    sshUser = "infrastructure";
-    # if the builder supports building for multiple architectures, 
-    # replace the previous line by, e.g.,
-    systems = [ "x86_64-linux" /*"aarch64-linux"*/ ];
-    maxJobs = 20;
-    speedFactor = 2;
-    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-    mandatoryFeatures = [ ];
-  }];
-  nix.maxJobs = 0;
-  nix.distributedBuilds = true;
-  nix.extraOptions = ''
-    builders-use-substitutes = true
-  '';
+  #  nix.buildMachines = [{
+  #    hostName = "100.107.101.14";
+  #    system = "x86_64-linux";
+  #    protocol = "ssh-ng";
+  #    sshUser = "build";#
+  #    sshKey = "/root/id_ed25519_builder";
+  # if the builder supports building for multiple architectures, 
+  # replace the previous line by, e.g.,
+  #    systems = [ "x86_64-linux" /*"aarch64-linux"*/ ];
+  #    maxJobs = 20;
+  #    speedFactor = 5;
+  #    supportedFeatures = [ "big-parallel" "kvm" ]; #   "nixos-test" "benchmark"
+  #    mandatoryFeatures = [ ];
+  #  },
+  #  {
+  #    hostName = "10.88.128.3"; #The Nas
+  #    system = "x86_64-linux";
+  #    protocol = "ssh-ng";
+  #    sshUser = "build";#
+  #sshKey = "/root/id_ed25519_builder";
+  # if the builder supports building for multiple architectures, 
+  # replace the previous line by, e.g.,
+  #    systems = [ "x86_64-linux" /*"aarch64-linux"*/ ];
+  #    maxJobs = 20;
+  #    speedFactor = 5;
+  #    supportedFeatures = [ "big-parallel" "kvm" ]; #   "nixos-test" "benchmark"
+  #    mandatoryFeatures = [ ];
+  #  }];
+  nix = {
+    settings = {
+      download-buffer-size = 524288000;
+      max-jobs = 50;
+      cores = 0;
+      nrBuildUsers = 50;
+      distributedBuilds = true;
+      extraOptions = ''
+        builders-use-substitutes = true
+      '';
+    };
+  };
 }
