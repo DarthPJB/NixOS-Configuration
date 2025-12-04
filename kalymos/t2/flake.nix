c6371f703a8150bb479d8c041644a2a2a3c1fe7a
John Bargman <darthpjb@gmail.com>
2025-12-04 23:01:23 +0000

    take 2

---
+++ kalymos/t2/flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      esp32-src = ./esp32-controller;  # Directory with platformio.ini and code
+    in {
+      packages.${system}.esp32-firmware = pkgs.stdenv.mkDerivation {
+        name = "esp32-firmware";
+        src = esp32-src;
+        buildInputs = [ pkgs.platformio ];
+        buildPhase = "platformio run";
+        installPhase = ''
+          mkdir -p $out
+          cp .pio/build/*/firmware.bin $out/
+        '';
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ pkgs.platformio ];
+      };
+
+      apps.${system}.flash = {
+        type = "app";
+        program = "${pkgs.writeScript "flash-esp32" ''
+          #!${pkgs.bash}/bin/bash
+          cd ${esp32-src}
+          platformio run --target upload
+        ''}";
+      };
+    };
+}
+
