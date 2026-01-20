1b9d6c6dcafe318ce300d1d172c9b1b92d71e532
John Bargman <john.bargman@platonic.systems>
2026-01-20 00:48:19 +0000

    integration complete

--- machines/alpha-one/default.nix
+++ machines/alpha-one/default.nix
@@ -12,8 +12,24 @@ in
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../lib/enable-wg.nix
+      ../../environments/i3wm_darthpjb.nix
+      ../../environments/steam.nix
+      ../../environments/code.nix
+      ../../environments/neovim.nix
+      ../../environments/communications.nix
+      ../../environments/browsers.nix
+      ../../environments/cad_and_graphics.nix
+      ../../environments/3dPrinting.nix
+      ../../environments/audio_visual_editing.nix
+      ../../environments/general_fonts.nix
+      ../../environments/video_call_streaming.nix
+      ../../environments/cloud_and_backup.nix
+      ../../environments/rtl-sdr.nix
+      ../../modifier_imports/bluetooth.nix
+      ../../modifier_imports/hosts.nix
+      ../../modifier_imports/cuda.nix
     ];
-
+services.xserver.videoDrivers = [ "nvidia" ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -25,5 +41,16 @@ in
       privateKeyFile = config.secrix.services.wireguard-wireg0.secrets."${hostname}".decrypted.path;
     };
 
+  hardware = {
+    sane.enable = true;
+    graphics.enable = true;
+    graphics.enable32Bit = true;
+    nvidia = {
+      nvidiaSettings = true;
+      open = false;
+      modesetting.enable = false;
+      powerManagement.enable = true;
+    };
+  };
 }
 
