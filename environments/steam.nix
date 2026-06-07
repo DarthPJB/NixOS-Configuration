d508f3e2ee5a9fd1df00bc6a89cffae7bc8cd959
John Bargman <john.bargman@platonic.systems>
2026-06-07 02:56:48 +0000

    fixxies

--- environments/steam.nix
+++ environments/steam.nix
@@ -17,4 +17,13 @@
     llm.prismlauncher
     unstable.vintagestory
   ];
+
+  # Register prismlauncher:// and curseforge:// URL scheme handlers
+  # so browsers can hand off OAuth callbacks to PrismLauncher
+  xdg.mime.enable = true;
+  environment.etc."xdg/mimeapps.list".text = ''
+    [Default Applications]
+    x-scheme-handler/prismlauncher=org.prismlauncher.PrismLauncher.desktop
+    x-scheme-handler/curseforge=org.prismlauncher.PrismLauncher.desktop
+  '';
 }
